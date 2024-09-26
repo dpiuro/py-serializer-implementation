98ad589d5fa94d0883074060d529794e04716c4d
Denys Piuro <denys.pyuro@gmail.com>
2024-09-26 17:32:32 +0300

    Fixed flake8

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(serializer.data).encode('utf-8')
+    return json.dumps(serializer.data).encode("utf-8")
+
 
 def deserialize_car_object(json_bytes: bytes) -> Car:
     data = json.loads(json_bytes.decode("utf-8"))
